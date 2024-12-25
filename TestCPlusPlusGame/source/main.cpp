5b36df16f909544d8f49277b64d0b5ee7dafca31
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2024-12-25 03:27:52 +0300

    Выбор цветного окна

--- TestCPlusPlusGame/source/main.cpp
+++ TestCPlusPlusGame/source/main.cpp
@@ -4,8 +4,19 @@
 #include <windows.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <iostream>
+#include <map>
+#include <string>
 
 static bool quit = false;
+static int prikoltype = 0;
+std::map<int, std::string> prikols = {
+        {0, "Fake Yellow (Red & Green)"},
+        {1, "Random Colors"},
+        {2, "Blue-violet Striped Gradient"},
+		{3, "Green Pixels"},
+		{4, "Yellow with Rare Purple"}
+    };
 
 struct {
     int width;
@@ -20,11 +31,34 @@ LRESULT CALLBACK WindowProcessMessage(HWND, UINT, WPARAM, LPARAM);
 #define Rand32() rand()
 #endif
 
+double Rand01() {
+	return (double)rand() / (double)RAND_MAX;
+}
+
+unsigned int ToColor(double red, double green, double blue) {
+    int r = static_cast<int>(red * 255);
+    int g = static_cast<int>(green * 255);
+    int b = static_cast<int>(blue * 255);
+
+    unsigned int color = 0xFF000000 | (r << 16) | (g << 8) | b;
+    return color;
+}
+
 static BITMAPINFO frame_bitmap_info;
 static HBITMAP frame_bitmap = 0;
 static HDC frame_device_context = 0;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow) {
+	int number;
+    do {
+        std::cout << "===- SELECT PRIKOL -===" << std::endl;
+		for(const auto& pair : prikols){
+			std::cout << pair.first << ": " << pair.second << std::endl;
+		}
+        std::cin >> number;
+    } while (number < 0 || number > prikols.size() - 1);
+	prikoltype = number;
+	
     const wchar_t window_class_name[] = L"My Window Class";
     static WNDCLASS window_class = { 0 };
     window_class.lpfnWndProc = WindowProcessMessage;
@@ -46,13 +80,58 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
         static MSG message = { 0 };
         while(PeekMessage(&message, NULL, 0, 0, PM_REMOVE)) { DispatchMessage(&message); }
 
-        static unsigned int p = 0;
-        //frame.pixels[(p++)%(frame.width*frame.height)] = Rand32();
-        //frame.pixels[Rand32()%(frame.width*frame.height)] = 0xFFFF0000;
-		for(int i = 0; i < std::round(frame.width*frame.height/2); i++) {
-			frame.pixels[Rand32()%(frame.width*frame.height)] = Rand32();
+		static unsigned int p = 0;
+		int width = frame.width;
+		int height = frame.height;
+		int framesize = width*height;
+		switch (prikoltype)
+		{
+			case 0:
+				for(int i = 0; i < 100; i++){
+					int pos = (p++)%(framesize);
+					if(pos%2==0){
+						frame.pixels[pos] = ToColor(1,0,0);
+					}else{
+						frame.pixels[pos] = ToColor(0,1,0);
+					}
+				}
+				break;
+			case 1:
+				for(int i = 0; i < 100; i++){
+					int pos = Rand32()%(framesize);
+					frame.pixels[pos] = Rand32();
+				}
+				break;
+			case 2:
+				for(int i = 0; i < 100; i++){
+					int pos = (p++)%(framesize);
+					double d = (double)pos/(double)framesize;
+					double d2 = (double)i/100;
+					frame.pixels[pos] = ToColor(d,0,d2);
+				}
+				break;
+			case 3:
+				for(int i = 0; i < 100; i++){
+					int pos = ((p++)*5)%(framesize);
+					double d = (double)pos/(double)framesize;
+					double d2 = (double)i/100;
+					frame.pixels[pos] = ToColor(0,std::abs(d-d2),0);
+				}
+				break;
+			case 4:
+				for(int i = 0; i < 100; i++){
+					p += 2;
+					int pos = p%(framesize);
+					if(Rand01() > 0.99){
+						frame.pixels[pos] = ToColor(1,0,1);
+					}else{
+						frame.pixels[pos] = ToColor(1,1,0);
+					}
+				}
+				break;
+			default:
+				break;
 		}
-
 
         InvalidateRect(window_handle, NULL, FALSE);
         UpdateWindow(window_handle);
@@ -60,7 +139,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
 
     return 0;
 }
-
 
 LRESULT CALLBACK WindowProcessMessage(HWND window_handle, UINT message, WPARAM wParam, LPARAM lParam) {
     switch(message) {
