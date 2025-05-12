6aee94db4416ce690e10ead0206bef0f0f0080c9
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2025-05-13 01:25:35 +0300

    Woowz-Test OS 2

--- OS/Orig/asm_library.asm
+++ OS/Orig/asm_library.asm
@@ -1,17 +1,21 @@
+; Цвет консоли
+console_color db 0x0F
+
 ; Написать символ [al] в консоли
 print_char:
-	mov ah, 0x0E
-	int 0x10
+	call print_char_nomove
+	inc dl
+	call set_input_position
 	ret
 
 ; Написать символ [al] в консоли, не двигая инпут
 print_char_nomove:
 	mov ah, 0x09
-    mov bl, 0x07
-    mov cx, 0x01
-    int 0x10
-    ret
-	
+	mov bl, [console_color]
+	mov cx, 0x01
+	int 0x10
+	ret
+
 print_nl:
 	mov al, 0x0D
 	call print_char
@@ -48,7 +52,7 @@ keypress:
 	mov ah, 0x00
 	int 0x16
 	ret
-	
+
 ; Обработка нажатия в консоли
 console_keypress:
 	push dx
@@ -75,6 +79,18 @@ console_keypress:
 	cmp ah, 0x4D
 	je .press_right
 	
+	; Доп. клавиши
+	cmp ah, 0x3B
+	je .press_f1
+	cmp ah, 0x3C
+	je .press_f2
+	cmp ah, 0x3D
+	je .press_f3
+	cmp ah, 0x3E
+	je .press_f4
+	cmp ah, 0x3F
+	je .press_f5
+	
 	call print_char_nomove
 	call .right
 	
@@ -98,6 +114,49 @@ console_keypress:
 	jmp .done
 .press_down:
 	call .down
+	jmp .done
+.press_f1:
+	call clear_console
+	jmp .done
+.press_f2:
+	dec byte [console_color]
+	call change_screen_color
+	jmp .done
+.press_f3:
+	inc byte [console_color]
+	call change_screen_color
+	jmp .done
+.press_f4:
+	dec byte [console_color]
+	push dx
+	
+	mov al, 1
+	mov dh, 1
+	mov dl, 0
+	call set_input_position
+	call print_char_nomove
+	mov dl, 79
+	call set_input_position
+	call print_char_nomove
+	
+	pop dx
+	call set_input_position
+	jmp .done
+.press_f5:
+	inc byte [console_color]
+	push dx
+	
+	mov al, 1
+	mov dh, 1
+	mov dl, 0
+	call set_input_position
+	call print_char_nomove
+	mov dl, 79
+	call set_input_position
+	call print_char_nomove
+	
+	pop dx
+	call set_input_position
 	jmp .done
 .next:
 	mov dl, 0
@@ -136,10 +195,88 @@ console_keypress:
 	ret
 .return:
 	ret
+
+; Очищает консоль
+string_startmessage db 'Welcome to Woowz-Test OS', 0
+clear_console:
+	call clear_screen
+
+	mov cx, 28
+	call .space
+	mov si, string_startmessage
+	call print_string
+	mov cx, 28
+	call .space
 	
+	mov al, 2
+	call print_char
+	mov cx, 78
+.line:
+	push cx
+	mov al, '='
+	call print_char
+	pop cx
+	dec cx
+	cmp cx, 0
+	jne .line
+	
+	mov al, 2
+	call print_char
+	
+	mov dl, 0
+	mov dh, 2
+	call set_input_position
+	
+	ret
+.space:
+	push cx
+	mov al, ' '
+	call print_char
+	pop cx
+	dec cx
+	cmp cx, 0
+	jne .space
+	
+	ret
+
 ; Очищает экран
 clear_screen:
+	mov dl, 0
+	mov dh, 0
+	call set_input_position
+
 	mov ah, 0x00
 	mov al, 0x03
 	int 0x10
+	ret
+
+; Изменить цвет одного символа, x [dl], y [dh]
+change_char_color:
+	call set_input_position
+	mov ah, 0x08
+	int 0x10
+	call print_char_nomove
+	ret
+
+; Изменить цвет всех символов на экране
+change_screen_color:
+	push dx
+
+	mov dl, -1
+	mov dh, -1
+	call set_input_position
+.cycle:
+	inc dl
+	call change_char_color
+	cmp dl, 79
+	jne .cycle
+	
+	mov dl, -1
+	inc dh
+	cmp dh, 25
+	jne .cycle
+	
+	pop dx
+	call set_input_position
+	
 	ret
--- OS/Orig/boot.asm
+++ OS/Orig/boot.asm
@@ -1,17 +1,9 @@
 org 0x7C00
 
 start_os:
-	call clear_screen
-
-	mov al, 1
-	call print_char
-	mov si, string_startmessage
-	call print_string
-	mov al, 1
-	call print_char
+	call clear_console
 	
-	mov si, string_line
-	call print_string
+	call change_screen_color
 	
 	jmp cycle_os
 	
@@ -23,9 +15,6 @@ cycle_os:
 	jmp cycle_os
 	
 %include "asm_library.asm"
-
-string_startmessage db  '                        Welcome to Woowz-Test OS!                             ', 0
-string_line db         '================================================================================'
 
 end_os:
 
